06d860f0ba7476f28f65a831ddb37e0f0d8325e1
Ganesh <149141995+ganesh-badar@users.noreply.github.com>
2025-12-16 10:33:25 +0530

    Add files via upload

---
+++ Hibernate/Table_Per_Class/src/mypack/Regular_Employee.java
@@ -0,0 +1,21 @@
+package mypack;
+
+public class Regular_Employee extends Employee
+{
+	private int salary;
+	private int bonus;
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getBonus() {
+		return bonus;
+	}
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	
+
+}
